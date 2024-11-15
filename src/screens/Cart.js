9ad226f0c5425a044f3a5968b971823775e3da20
peakub <159338652+peakub@users.noreply.github.com>
2024-11-15 11:44:57 +0700

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -14,7 +14,7 @@ export default function Cart() {
 
     const handleCheckOut = async () => {
         let userEmail = localStorage.getItem("userEmail");
-        let response = await fetch("http://localhost:8080/api/orderData", {
+        let response = await fetch("http://localhost:10000/api/orderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
