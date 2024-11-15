da08cbc01a63047f8199c67a775b150233f0e808
peakub <159338652+peakub@users.noreply.github.com>
2024-11-15 11:45:30 +0700

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:8080/api/myOrderData", {
+        await fetch("http://localhost:10000/api/myOrderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
